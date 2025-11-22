ca39bb98ab5c3c180d0acdce35d611ded8350b34
bscitdrive <bscitdrive2023@gmail.com>
2025-11-22 17:02:58 +0530

    Add files via upload

---
+++ prac3/Question1.java
@@ -0,0 +1,45 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Question1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver","E:\\Sem_3\\STQA\\Selenium_setup\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+
+		//Open ToolsQA Selenium Training page
+		driver.get("https://www.toolsqa.com/selenium-training/");
+		Thread.sleep(2000);
+
+		//Click on Registration button link (using CSS Selector)
+		driver.findElement(By.cssSelector("a[href='#enroll-form']")).click();
+		System.out.println("Clicked on Registration button link");
+		Thread.sleep(2000);
+
+		//Come back to Home page (Back command)
+		driver.navigate().back();
+		System.out.println("Come back to Home page");
+		Thread.sleep(2000);
+
+		//Go forward to Registration page (Forward command)
+		driver.navigate().forward();
+		System.out.println("Go forward to Registration page");
+		Thread.sleep(2000);
+
+		//Come back to Home page (To command)
+		driver.navigate().to("https://www.toolsqa.com/selenium-training/");
+		System.out.println("Come back to Home page");
+		Thread.sleep(2000);
+
+		//Refresh the Browser (Refresh command)
+		driver.navigate().refresh();
+		System.out.println("Refresh the Browser");
+		Thread.sleep(2000);
+
+		//Close the browser
+		driver.quit();
+	}
+
+}
---
+++ prac3/Question_2.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Question_2 {
+
+	public static void main(String[] args) throws InterruptedException {
+	    // a) Invoke Firefox Browser
+	    System.setProperty("webdriver.gecko.driver","E:\\Sem_3\\STQA\\Selenium_setup\\geckodriver.exe");
+	    WebDriver driver = new FirefoxDriver();
+	    
+	    // b) Open URL: https://www.google.co.in/
+	    driver.get("https://www.google.co.in/");
+	    Thread.sleep(2000);
+
+	    // c) Get Page Title name and Title length
+	    String title = driver.getTitle();
+	    int titleLength = title.length();
+
+	    // d) Print Page Title and Title length on the Eclipse Console
+	    System.out.println("Page Title: " + title);
+	    System.out.println("Title Length: " + titleLength);
+
+	    // e) Get page URL and verify whether it is the desired page or not
+	    String actualUrl = driver.getCurrentUrl();
+	    String expectedUrl = "https://www.google.co.in/";
+	    if (actualUrl.contains("google.co"))
+	        System.out.println("URL Verification Passed: " + actualUrl);
+	    else
+	        System.out.println("URL Verification Failed! Actual: " + actualUrl);
+
+	    // f) Get page Source and Page Source length
+	    String pageSource = driver.getPageSource();
+	    int pageSourceLength = pageSource.length();
+
+	    // g) Print page Length on Eclipse Console
+	    System.out.println("Page Source Length: " + pageSourceLength);
+
+	    // h) Close the Browser
+	    driver.quit();
+	}
+
+
+}
